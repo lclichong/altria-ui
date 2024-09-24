fa29bfb102be45605d4646918c2f79ec9c6191d0
li chong <736117210@qq.com>
2024-09-24 16:57:26 +0800

    chore: fix vue.config.js

--- packages/altria-cli/vue.config.js
+++ packages/altria-cli/vue.config.js
@@ -3,6 +3,7 @@ const { resolve } = require('path')
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '././' : './',
     productionSourceMap: process.env.NODE_ENV !== 'production',
+    parallel: false,
     configureWebpack: {
         module: {
             rules: [
